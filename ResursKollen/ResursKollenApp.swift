d08c73162d17344d36f9071e6472a4aa0dd14cc0
angie <angelica.ekman@hotmail.com>
2025-05-14 13:29:16 +0200

    Firebase, gitignore

--- ResursKollen/ResursKollenApp.swift
+++ ResursKollen/ResursKollenApp.swift
@@ -1,17 +1,27 @@
-//
-//  ResursKollenApp.swift
-//  ResursKollen
-//
-//  Created by Angelica E on 2025-05-14.
-//
-
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ResursKollenApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
