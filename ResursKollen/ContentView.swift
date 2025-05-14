e8c04b99a9dac45c63a974d4c3fdd608fae28018
angie <angelica.ekman@hotmail.com>
2025-05-14 13:05:22 +0200

    Initial Commit

---
+++ ResursKollen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ResursKollen
+//
+//  Created by Angelica E on 2025-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ResursKollen/ResursKollenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResursKollenApp.swift
+//  ResursKollen
+//
+//  Created by Angelica E on 2025-05-14.
+//
+
+import SwiftUI
+
+@main
+struct ResursKollenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
