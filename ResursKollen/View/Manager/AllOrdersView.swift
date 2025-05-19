187e8163fc5bb815bb8424114768b3c36d5fee8c
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-05-19 20:10:26 +0200

    la till en lyssnare för Delayed ordrar

--- ResursKollen/View/Manager/AllOrdersView.swift
+++ ResursKollen/View/Manager/AllOrdersView.swift
@@ -29,16 +29,24 @@ extension AllOrdersView{
         private let firestore = FirestoreManager()
         
         @Published var orders : [Order] = []
-        
+        @Published var delayedOrders : [Order] = []
         //Lyssnar direkt vi initierar viewmodel
         init() {
             listenToOrderCollection()
+            listenToDelayed()
         }
         // Updaterar UI på maintråden
         func listenToOrderCollection() {
             firestore.listenToOrderCollection { [weak self] newOrders in
                 DispatchQueue.main.async {
                     self?.orders = newOrders
+                }
+            }
+        }
+        func listenToDelayed() {
+            firestore.listenToDelayed { [weak self ] newOrders in
+                DispatchQueue.main.async {
+                    self?.delayedOrders = newOrders
                 }
             }
         }
