cbc6e5e2a525e5f002411399d172b6b647c4840a
angie <angelica.ekman@hotmail.com>
2025-05-19 15:55:30 +0200

    created staffView UI

--- ResursKollen/View/StaffView.swift
+++ ResursKollen/View/StaffView.swift
@@ -1,18 +1,50 @@
-//
-//  StaffView.swift
-//  ResursKollen
-//
-//  Created by Magnus Freidenfelt on 2025-05-16.
-//
+
 
 import SwiftUI
 
 struct StaffView: View {
+    
+    @State var user: UserData
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color(.systemGray6), Color(.systemGray5)]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            ).edgesIgnoringSafeArea(.all)
+
+            NavigationView {
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    List {
+                        HStack {
+                            
+                                VStack(alignment: .leading) {
+                                    Text("\(user.name)")
+                                        .font(.headline)
+                                    
+                                    Text("\(user.phoneNumber)")
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.orange)
+                            }.listStyle(.insetGrouped)
+                        }
+                    .listStyle(.insetGrouped)
+                    .navigationTitle("Anställda")
+                }
+
+                    
+        
+            }
+        }
     }
 }
 
 #Preview {
-    StaffView()
+    StaffView(
+        user: UserData(id: "1", status: .employee, name: "Vivianne och Angie", employmentDate: Date(), employmentNumber: "EMP123", phoneNumber: "0701234567"))
 }
