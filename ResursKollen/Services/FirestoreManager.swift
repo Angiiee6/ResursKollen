e5eed9d29edd4be46fe1831e2c03187a16dd8e60
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-05-19 13:53:11 +0200

    Switching views

--- ResursKollen/Services/FirestoreManager.swift
+++ ResursKollen/Services/FirestoreManager.swift
@@ -8,9 +8,14 @@
 import FirebaseFirestore
 
 class FirestoreManager {
+    @Published var orders : [Order] = []
+    
+    static let shared = FirestoreManager()
+    
     let db = Firestore.firestore()
 
     var orderRef: CollectionReference { db.collection("orders") }
+    
 
     func saveOrder(_ order: Order) async throws {
         let newDocument = orderRef.document()
@@ -19,5 +24,25 @@ class FirestoreManager {
         try orderRef.document(newDocument.documentID).setData(
             from: updatedOrder
         )
+    }
+    
+    func listenToOrderCollection() {
+        
+        orderRef.addSnapshotListener {snapshot, error in
+            if let error = error {
+                print("Error listening to Orders: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let documents = snapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.orders = documents.compactMap { doc in
+                try? doc.data(as: Order.self)
+            }
+            
+        }
     }
 }
--- ResursKollen/View/Manager/AllOrdersView.swift
+++ ResursKollen/View/Manager/AllOrdersView.swift
@@ -9,10 +9,50 @@ import SwiftUI
 
 struct AllOrdersView: View {
     var body: some View {
-        Text("Alla ordrar listade")
+        NavigationStack {
+            VStack {
+                
+            }
+        }
     }
 }
 
 #Preview {
     AllOrdersView()
 }
+
+extension AllOrdersView{
+    
+    class AllOrdersViewModel: ObservableObject {
+        private let firestore = FirestoreManager()
+        
+        @Published var orders : [Order] = []
+        
+        init() {
+            listenToOrderCollection()
+        }
+        
+        func listenToOrderCollection() {
+            let orderRef = firestore.orderRef
+            
+            orderRef.addSnapshotListener {snapshot, error in
+                if let error = error {
+                    print("Error listening to Orders: \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let documents = snapshot?.documents else {
+                    print("No documents")
+                    return
+                }
+                
+                self.orders = documents.compactMap { doc in
+                    try? doc.data(as: Order.self)
+                }
+                
+            }
+        }
+        
+    }
+}
+
