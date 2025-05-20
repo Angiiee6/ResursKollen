dd8594ed1bbc1c8a018754ee849e4deb255009e7
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-05-20 09:08:55 +0200

    sdf

---
+++ ResursKollen/View/Components/OrderCardView.swift
@@ -0,0 +1,17 @@
+//
+//  OrderCardView.swift
+//  ResursKollen
+//
+//  Created by Robin jakobsson on 2025-05-19.
+//
+
+import SwiftUI
+
+struct OrderCardView: View {
+    let order : Order?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
