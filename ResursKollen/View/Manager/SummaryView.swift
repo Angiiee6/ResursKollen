ad6b37fab1f2b97949b40fd1ed1fe05662304253
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-05-20 10:53:05 +0200

    Creating Chart for summaryTab

--- ResursKollen/View/Manager/SummaryView.swift
+++ ResursKollen/View/Manager/SummaryView.swift
@@ -6,13 +6,43 @@
 //
 
 import SwiftUI
+import Charts
 
 struct SummaryView: View {
+    
+    let dummyOrders : [String] = ["Hej","Hejehej","Robin","HAERKJAH"]
+    
     var body: some View {
-        Text("Graf och statistik här")
+        VStack {
+            
+          
+                
+            }
+            
+        }
     }
-}
+
 
 #Preview {
     SummaryView()
+}
+
+extension SummaryView {
+    
+    class SummaryViewModel: ObservableObject {
+        let db = FirestoreManager()
+        
+        @Published var orders : [Order] = []
+        
+        func GetOrders() {
+            db.listenToOrderCollection { [weak self ] newOrders in
+                DispatchQueue.main.async {
+                    self?.orders = newOrders
+                }
+                
+            }
+        }
+        
+        
+    }
 }
