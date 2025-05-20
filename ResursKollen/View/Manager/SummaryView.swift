9ad4ee9557b620e642af4ed615157913fee1c401
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-05-20 11:24:34 +0200

    Bytte till Vår standard mörka bakgrund och ändrade färg på allt annat

--- ResursKollen/View/Manager/SummaryView.swift
+++ ResursKollen/View/Manager/SummaryView.swift
@@ -13,26 +13,58 @@ struct SummaryView: View {
     let dummyOrders : [String] = ["Hej","Hejehej","Robin","HAERKJAH"]
     @StateObject var vm = SummaryViewModel()
     var body: some View {
-        VStack {
-            Text("Orderöversikt")
-                .font(.title)
-                .bold()
-            
-            Chart {
-                ForEach(vm.chartData) {data in
-                    BarMark(
-                        x: .value("Status", data.status),
-                        y: .value("Antal", data.count)
-                    )
-                    .foregroundStyle(.blue)
+        ZStack {
+            //Bakgrund
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color(red: 0.11, green: 0.11, blue: 0.15),
+                    Color(red: 0.20, green: 0.20, blue: 0.25),
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            ).edgesIgnoringSafeArea(.all)
+            // Titel
+            VStack {
+                Text("Orderöversikt")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .bold()
+                // Chart
+                Chart {
+                    ForEach(vm.chartData) {data in
+                        BarMark(
+                            x: .value("Status", data.status),
+                            y: .value("Antal", data.count)
+                        )
+                        .foregroundStyle(.blue)
+                        
+                    }
+                }
+                // X Led
+                .chartXAxis {
+                    AxisMarks {
+                        AxisGridLine()
+                        AxisTick()
+                        AxisValueLabel()
+                            .foregroundStyle(.white)
+                            .font(.caption)
+                    }
+                }
+                // Y led
+                .chartYAxis {
+                    AxisMarks {
+                        AxisGridLine()
+                        AxisTick()
+                        AxisValueLabel()
+                            .foregroundStyle(.white)
+                    }
+                }
+                .frame(height: 200)
+                .padding()
+                Spacer()
+                
                 }
             }
-            .frame(height: 200)
-            .padding()
-            Spacer()
-            
-            }
-            
         }
     }
 
