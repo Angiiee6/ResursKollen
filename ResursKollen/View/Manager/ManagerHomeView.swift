fce72c1fd5588aed7080fe8d3134f0be1ea8f387
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-05-28 13:54:22 +0200

    Commit

--- ResursKollen/View/Manager/ManagerHomeView.swift
+++ ResursKollen/View/Manager/ManagerHomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ManagerHomeView: View {
     let currentUser: UserData
 
+    @State private var isLoggedOut = false
     ///EXEMPEL USER
     let exampleUser = UserData(
         id: "1",
@@ -21,40 +22,71 @@ struct ManagerHomeView: View {
     )
 
     var body: some View {
-        TabView {
-            NavigationStack {
-                ManagerAllOrdersView()
-            }
-            .tabItem {
-                Label("Aktiva ordrar", systemImage: "list.bullet.clipboard")
-            }
+        NavigationStack{
             
-
-            NavigationStack {
+            NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true), isActive: $isLoggedOut){
+                EmptyView()
+            }.navigationBarBackButtonHidden(true)
+            
+            TabView {
+                
+                ManagerAllOrdersView()
+                
+                    .tabItem {
+                        Label("Aktiva ordrar", systemImage: "list.bullet.clipboard")
+                    }
+                
                 ReviewOrdersView()
-            }
-            .tabItem {
-                Label("Utförda ordrar", systemImage: "text.page.badge.magnifyingglass")
-            }
-
-            NavigationStack {
+                
+                    .tabItem {
+                        Label(
+                            "Utförda ordrar",
+                            systemImage: "text.page.badge.magnifyingglass"
+                        )
+                    }
+                
                 SummaryView()
+                
+                
+                    .tabItem {
+                        Label(
+                            "Statistik",
+                            systemImage: "waveform.badge.magnifyingglass"
+                        )
+                    }
+                
+                StaffView(currentUser: currentUser)
+                
+                    .tabItem {
+                        Label("Personal", systemImage: "person.3")
+                    }
+                NewMessageEditView()
+                    .tabItem{
+                        Label("Meddlanden", systemImage: "message")
+                    }
+                
             }
-            .tabItem {
-                Label("Statistik", systemImage: "waveform.badge.magnifyingglass")
-            }
-
-            NavigationStack {
-                StaffView(currentUser: currentUser )
-            }
-            .tabItem {
-                Label("Personal", systemImage: "person.3")
+            .tint(Color.orange)
+            .toolbar{
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button{
+                        do{
+                            try AuthenticationManager.shared.signOut()
+                            isLoggedOut = true
+                        }catch {
+                            print("Kunde inte logga ut användaren")
+                        }
+                    }label: {
+                     Image(systemName: "rectangle.portrait.and.arrow.right")
+                            .tint(.orange)
+                    }
                 }
             }
-        .tint(Color.orange)
         }
+       
     }
-
+    
+}
 
 //TODO: Fetch all orders here instead of in sub-views
 
