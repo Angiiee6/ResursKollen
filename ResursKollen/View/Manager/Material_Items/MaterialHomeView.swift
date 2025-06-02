1dffc1607c503192c3e31465ef43dbc19601c870
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-06-02 14:34:47 +0200

    [Feature] Added Material / Item home view

---
+++ ResursKollen/View/Manager/Material_Items/MaterialHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  MaterialHomeView.swift
+//  ResursKollen
+//
+//  Created by Daniel A on 2025-06-02.
+//
+
+import SwiftUI
+
+struct MaterialHomeView: View {
+    var body: some View {
+        Text("här kommer det material")
+    }
+}
+
+#Preview {
+    MaterialHomeView()
+}
