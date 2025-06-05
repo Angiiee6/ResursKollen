4a04395ba8accd61220569413e98b290bd84afa9
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2025-06-05 12:14:10 +0200

    Fixes the UI for search suggestions.

--- ResursKollen/View/Manager/CustomerView/Sheets/AddCustomerSheet.swift
+++ ResursKollen/View/Manager/CustomerView/Sheets/AddCustomerSheet.swift
@@ -13,6 +13,8 @@ struct AddCustomerSheet: View {
     @StateObject var viewModel = ViewModel()
 
     @State var alertIsPresent = false
+    @State var showSuggestions = false
+    @FocusState var streetNameTextFieldIsFocused: Bool
 
     @State var name: String = ""
     @State var phone: String = ""
@@ -20,30 +22,70 @@ struct AddCustomerSheet: View {
 
     var body: some View {
         VStack {
-            Form {
-                Section("Kundinformation") {
-                    TextField("Namn", text: $name)
-                    TextField("Gata", text: $viewModel.streetName)
-                    TextField("Postkod", text: $viewModel.postalCode)
-                    TextField("Stad", text: $viewModel.city)
-                    TextField("Telefonnummer", text: $phone)
-                    TextField("Emailadress", text: $email)
+            Text("Lägg till ny kund")
+                .font(.title)
+            HStack {
+                VStack(alignment: .leading, spacing: 16) {
+                    if !showSuggestions {
+                        VStack(alignment: .leading) {
+                            Text("Namn: *")
+                                .font(.caption)
+                            TextField("Namn", text: $name)
+                        }
+                    }
+                    VStack(alignment: .leading) {
+                        Text("Adress: *")
+                            .font(.caption)
+                        VStack(alignment: .leading) {
+                            TextField("Gata", text: $viewModel.searchText)
+                                .focused($streetNameTextFieldIsFocused)
+                                .onSubmit {
+                                    animateSuggestions(shown: false)
+                                }
+                            if !showSuggestions {
+                                TextField(
+                                    "Postkod",
+                                    text: $viewModel.postalCode
+                                )
+                                TextField("Stad", text: $viewModel.city)
+                            }
+                        }
+                        .font(.subheadline)
+                    }
+                    if !showSuggestions {
+                        VStack(alignment: .leading) {
+                            Text("Telefonnummer: *")
+                                .font(.caption)
+                            TextField("Telefonnummer", text: $phone)
+                                .keyboardType(.phonePad)
+                        }
+                        VStack(alignment: .leading) {
+                            Text("Email:")
+                                .font(.caption)
+                            TextField("Email", text: $email)
+                                .keyboardType(.emailAddress)
+                                .autocapitalization(.none)
+                        }
+                    }
                 }
+                Spacer()
             }
-            .onChange(of: viewModel.searchText) { _, newValue in
-                viewModel.updateSearchQuery(newValue)
+            .padding()
+            .onChange(of: viewModel.searchText) { _, newSearchString in
+                animateSuggestions(shown: !newSearchString.isEmpty)
+                viewModel.updateSearchQuery(newSearchString)
             }
-            Text("Sök adress:")
-            TextField("Adress", text: $viewModel.searchText)
-            if !viewModel.searchSuggestions.isEmpty {
+            .onChange(of: streetNameTextFieldIsFocused) { _, isFocused in
+                self.animateSuggestions(shown: false)
+            }
+            if showSuggestions {
                 VStack {
-                    Text("Välj adress:")
-                    List(viewModel.searchSuggestions, id: \.self) { suggestion in
+                    List(viewModel.searchSuggestions, id: \.self) {
+                        suggestion in
                         Button {
                             viewModel.selectAddress(suggestion)
-                            viewModel.searchText.removeAll()
-                        }
-                        label: {
+                            animateSuggestions(shown: false)
+                        } label: {
                             VStack(alignment: .leading) {
                                 Text(suggestion.title)
                                 Text(suggestion.subtitle)
@@ -52,25 +94,34 @@ struct AddCustomerSheet: View {
                             }
                         }
                     }
+                    Spacer()
                 }
             }
             Spacer()
-            Button("Spara") {
-                Task {
-                    let newCustomer = Customer(
-                        name: name,
-                        phoneNumber: phone,
-                        streetName: viewModel.streetName,
-                        city: viewModel.city,
-                        postalCode: viewModel.postalCode,
-                        emailAddress: email
-                    )
-                    await viewModel.saveCustomer(newCustomer)
-                    alertIsPresent = true
+            if !showSuggestions {
+                Button("Spara") {
+                    Task {
+                        let newCustomer = Customer(
+                            name: name,
+                            phoneNumber: phone,
+                            streetName: viewModel.searchText,
+                            city: viewModel.city,
+                            postalCode: viewModel.postalCode,
+                            emailAddress: email
+                        )
+                        await viewModel.saveCustomer(newCustomer)
+                        alertIsPresent = true
+                    }
                 }
+                .disabled(
+                    name.isEmpty || viewModel.searchText.isEmpty
+                    || viewModel.postalCode.isEmpty || viewModel.city.isEmpty
+                    || phone.isEmpty
+                )
+                .buttonStyle(.borderedProminent)
             }
-            .buttonStyle(.borderedProminent)
         }
+        .padding()
         .alert("Meddelande", isPresented: $alertIsPresent) {
             Button("Ok") {
                 dismiss()
@@ -81,6 +132,13 @@ struct AddCustomerSheet: View {
         }
 
     }
+
+    private func animateSuggestions(shown: Bool) {
+        withAnimation(.easeInOut(duration: 0.5)) {
+            showSuggestions = shown
+        }
+
+    }
 }
 
 //MARK: View model
@@ -88,57 +146,59 @@ struct AddCustomerSheet: View {
 extension AddCustomerSheet {
 
     class ViewModel: NSObject, ObservableObject {
-        
+
         @Published var searchText: String = ""
         @Published var searchSuggestions: [MKLocalSearchCompletion] = []
-        
-        @Published var streetName: String = ""
+
+        //@Published var streetName: String = ""
         @Published var postalCode: String = ""
         @Published var city: String = ""
-        
+
         private var completer = MKLocalSearchCompleter()
-        
+
         var alertMessage: String = ""
-        
+
         override init() {
             super.init()
             completer.resultTypes = .address
             completer.delegate = self
         }
-        
+
         func saveCustomer(_ customer: Customer) async {
             do {
                 try await FirestoreManager.shared.saveCustomer(customer)
                 alertMessage = "Ny kund tillagd!"
             } catch {
                 alertMessage =
-                "Något gick fel - kund kunde inte sparas: \(error.localizedDescription)"
+                    "Något gick fel - kund kunde inte sparas: \(error.localizedDescription)"
             }
         }
-        
+
         func updateSearchQuery(_ query: String) {
             searchText = query
             completer.queryFragment = query
-            if query.isEmpty {
-                searchSuggestions.removeAll()
-            }
+            //            if query.isEmpty {
+            //                searchSuggestions.removeAll()
+            //            }
         }
-        
+
         func selectAddress(_ completion: MKLocalSearchCompletion) {
             let searchRequest = MKLocalSearch.Request(completion: completion)
             let search = MKLocalSearch(request: searchRequest)
             search.start { response, error in
-                guard let placemark = response?.mapItems.first?.placemark, error == nil else {
+                guard let placemark = response?.mapItems.first?.placemark,
+                    error == nil
+                else {
                     print("Error: \(error?.localizedDescription ?? "No data")")
                     return
                 }
                 let street = placemark.thoroughfare ?? ""
                 let number = placemark.subThoroughfare ?? ""
-                self.streetName =  "\(street) \(number)"
+                self.searchText = "\(street) \(number)"
                 self.postalCode = placemark.postalCode ?? ""
                 self.city = placemark.locality ?? ""
             }
-            
+
         }
     }
 
@@ -156,7 +216,7 @@ extension AddCustomerSheet.ViewModel: MKLocalSearchCompleterDelegate {
     ) {
         print("Address search error: \(error.localizedDescription)")
     }
-    
+
 }
 
 #Preview {
