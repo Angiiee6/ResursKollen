2fbab2d342416b184086d2a0662f5a09f6259f9c
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-06-09 17:14:47 +0200

    [Feature] Moved customer button

--- ResursKollen/View/Manager/ManagerAllOrdersView.swift
+++ ResursKollen/View/Manager/ManagerAllOrdersView.swift
@@ -77,11 +77,12 @@ struct ManagerAllOrdersView: View {
                 
                 HStack{
                     
+                   /* ligger under flervalsknap
                     NavigationLink(destination: CustomerView()) {
                         Text("Kundvy (flytta sen)")
                             .foregroundStyle(.pink)
                         
-                    }
+                    } */
                     
 //                    Button(action: {
 //                        isCreateOrder = true
