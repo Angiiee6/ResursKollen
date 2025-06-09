3c181f5690fb0e67fd2a32cd74f449a7b0faf0cf
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-06-09 16:14:47 +0200

    [Feature] Material manger added

---
+++ ResursKollen/Services/MaterialManger.swift
@@ -0,0 +1,57 @@
+//
+//  MaterialManger.swift
+//  ResursKollen
+//
+//  Created by Daniel A on 2025-06-02.
+//
+
+import Foundation
+import FirebaseFirestore
+
+final class MaterialManager {
+    
+    static let shared = MaterialManager()
+    private init() {}
+    
+    
+    
+    private let materialColletion = Firestore.firestore().collection("material")
+    
+    func materialDocuments(material: Material) -> DocumentReference {
+        return materialColletion.document(material.id)
+    }
+    
+    //Ny post med förbruknings material
+    func writeNewMaterialPost(material: Material)async throws{
+        try materialDocuments(material: material).setData(from: material, merge: false)
+    }
+    
+    //hämta allt material
+    func readMaterialList() async throws -> [Material] {
+        
+        let snapshot = try await Firestore.firestore().collection("material").getDocuments()
+        
+            let material = snapshot.documents.compactMap { doc in
+                try? doc.data(as: Material.self)
+            }
+            return material
+    }
+    
+    
+    //Uppdatera befintlig post
+    func updateMaterialPost(material: Material) throws{
+        try? materialDocuments(material: material).setData(from: material, merge: true)
+    }
+    
+    
+    //radera post med material
+    func deleteMaterialPost(materialPost: Material)async throws{
+        
+        try await materialColletion.document(materialPost.id).delete()
+        
+        
+    }
+    
+    
+}
+
